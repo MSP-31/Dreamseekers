c845c7293da403f1ebbe4ca578f9af97edcfba41
MSP-31 <jhss9747@gmail.com>
2024-06-27 12:42:28 +0900

    ALLOWED_HOSTS 수정

--- dreamseekers/dreamseekers/settings.py
+++ dreamseekers/dreamseekers/settings.py
@@ -40,7 +40,7 @@ AUTH_USER_MODEL = 'user.Users'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1',]
+ALLOWED_HOSTS = ['0.0.0.0', 'website']
 
 # Application definition
 INSTALLED_APPS = [
